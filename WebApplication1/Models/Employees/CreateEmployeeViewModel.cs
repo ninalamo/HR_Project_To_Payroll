bac54a8de7d161ef726a78c411d1530862c53b93
Nin Alamo <nino.alamo@kmc.solutions>
2020-03-06 14:48:35 +0800

    Changed skin

--- WebApplication1/Models/Employees/CreateEmployeeViewModel.cs
+++ WebApplication1/Models/Employees/CreateEmployeeViewModel.cs
@@ -11,14 +11,18 @@ namespace WebApplication1.Models.Employees
         [Display(Name ="Employee Number")]
         public string EmployeeNumber { get; set; }
         [Required]
+        [Display(Name ="First Name")]
         public string FirstName { get; set; }
         [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         [Required]
         [EmailAddress]
+        [Display(Name = "Company Email")]
         public string CompanyEmail { get; set; }
         [Required]
         [EmailAddress]
+        [Display(Name = "Personal Email")]
         public string PersonalEmail { get; set; }
         public string CreatedBy { get; set; }
     }
